b3d61b71fcc24c86882479234c25e97931da526c
Thomas Iseli <thomas.iseli@webyourself.ch>
2023-07-05 14:46:13 +0200

    typo

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import "./style.css";
 import PButton from "./components/PButton.vue";
 import PLoader from "./components/PLoader.vue";
 import PCard from "./components/PCard.vue";
-import PTabs from "./components/PCard.vue";
+import PTabs from "./components/PTabs.vue";
 import PTabsButton from "./components/PTabsButton.vue";
 
 export { PButton, PLoader, PCard, PTabs, PTabsButton };
