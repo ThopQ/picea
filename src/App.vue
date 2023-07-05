ea85a12b0cecbf2138a2a11249149e0a4465e202
Thomas Iseli <thomas.iseli@webyourself.ch>
2023-07-05 14:32:57 +0200

    removed unnecessary files

--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import PButton from "./components/PButton.vue";
-</script>
-
-<template>
-  <h1 class="text-5xl font-black">Hi, this is Picea 👋🏼</h1>
-  <PButton>Test</PButton>
-</template>
