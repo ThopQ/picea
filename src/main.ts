29b68ec621d1c8968c5e6457c2e539ffa47df7dd
Thomas Iseli <ti@netivity.ch>
2023-07-07 16:03:28 +0200

    export input component

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ import PLoader from "./components/PLoader.vue";
 import PCard from "./components/PCard.vue";
 import PTabs from "./components/PTabs.vue";
 import PTabsButton from "./components/PTabsButton.vue";
+import PInput from "./components/PInput.vue";
 
-export { PButton, PLoader, PCard, PTabs, PTabsButton };
+export { PButton, PLoader, PCard, PTabs, PTabsButton, PInput };
