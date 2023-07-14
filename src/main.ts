8f934282efd8d5376036f321fb9673d6d4ddced7
Thomas Iseli <ti@netivity.ch>
2023-07-14 14:23:33 +0200

    added p-switch

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,16 @@ import PCard from "./components/PCard.vue";
 import PTabs from "./components/PTabs.vue";
 import PTabsButton from "./components/PTabsButton.vue";
 import PRadioGroup from "./components/PRadioGroup.vue";
+import PSwitch from "./components/PSwitch.vue";
 import PInput from "./components/PInput.vue";
 
-export { PButton, PLoader, PCard, PTabs, PTabsButton, PRadioGroup, PInput };
+export {
+  PButton,
+  PLoader,
+  PCard,
+  PTabs,
+  PTabsButton,
+  PRadioGroup,
+  PSwitch,
+  PInput,
+};
